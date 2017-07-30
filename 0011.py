fa5ac8795974ad0f801c594091a77beef77b4d46
polynomia <wyrsjtu@163.com>
2017-07-31 00:44:02 +0800

    finish 0011

--- 0011.py
+++ 0011.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # 第 0011 题： 敏感词文本文件 filtered_words.txt，当用户输入敏感词语时，则打印出 Freedom，否则打印出 Human Rights。
 
-import codecs
+import codecs, re
 
 
 def get_words(filepath, word_list):
@@ -11,8 +11,24 @@ def get_words(filepath, word_list):
             word_list.append(w.strip())
 
 
-if __name__ == '__main__':
-    path = 'source/0011/filtered_words.txt'
+def main():
+    path = 'source/filtered_words.txt'
     word_list = []
     get_words(path, word_list)
-    print(word_list)
+    while True:
+        print('pls input your words(input exit for exiting):')
+        words = input()
+        if words == 'exit':
+            break
+        flag = True
+        for word in word_list:
+            reTxt = re.compile(r'' + word + '')
+            if len(reTxt.findall(words)) != 0:
+                print('Freedom')
+                flag = False
+                break
+        if flag: print('Human Rights')
+
+
+if __name__ == '__main__':
+    main()
